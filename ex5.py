0da5b42f47e803c40bb09f51f5098a96a6709ac3
Cicei <cicei131@qq.com>
2016-12-25 19:03:15 +0800

    python text

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Mook A. Zeng'
+my_age = 28 # not a lie
+my_height = 66 # inches
+my_weight = 123 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the tea." % my_teeth
+
+# this line is tricky ,try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age+my_height+my_weight)
